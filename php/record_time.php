2676d00327fafdba9e1f5b484fa40e01bbe8c8da
jakapatb <38038597+jakapatb@users.noreply.github.com>
2018-04-16 01:01:46 +0100

    กด finish ไม่เด้งไปหน้าindex

--- php/record_time.php
+++ php/record_time.php
@@ -8,7 +8,7 @@ if(isset($_SESSION['coursedate'])){
 $strSQL = "UPDATE coursedate SET duration = '".$record."' WHERE ID = '".$_SESSION["coursedate"]."' ";
         $objQuery = mysqli_query($objCon,$strSQL);
 }
-header("location:../index.php");
+header("Location: {$_SERVER['HTTP_REFERER']}");
   mysqli_close($objCon);
    
 ?>
